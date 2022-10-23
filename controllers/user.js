e50c1f3645442024df0c425ca212e4095e09b76a
Jimmy Gorecki <jimmy27.gorecki01@gmail.com>
2022-10-24 00:50:44 +0200

    maj

--- controllers/user.js
+++ controllers/user.js
@@ -53,4 +53,4 @@ exports.login = (req, res, next) => {
                 .catch(error => res.status(500).json({ error }));
         })
         .catch(error => res.status(500).json({ error }));
- };
+};
